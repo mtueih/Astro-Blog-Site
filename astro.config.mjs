119c45c710799e8d98da4886cb354aff1420f33e
mtueih <mtueih.org@outlook.com>
2025-10-17 12:44:42 +0800

    feat(astro-config): 添加 astro-icon 插件支持
    引入 astro-icon 插件并配置 lucide 图标库，用于项目中图标资源的管理与使用。同时更新依赖项，添加 astro-icon 和 @iconify-json/lucide 包。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+// 图标
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.mtueih.com',
     trailingSlash: 'always',
+
+
+	// 插件
+	integrations: [
+		icon({
+			include: {
+				lucide: ['*'],
+			}
+		})
+	]
 });
